8c34470d056554a159c687f125be10b91be07099
chris <chr33s@icloud.com>
2021-03-12 13:11:30 +0700

    add: postcss-url

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,17 @@ module.exports = {
     cssnano: {
       preset: "default",
     },
-    tailwindcss: {},
     "postcss-each": {},
     "postcss-import": {},
     "postcss-nested": {},
     "postcss-preset-env": {},
+    "postcss-url": [
+      {
+        filter: "**/*.{gif,jpg,png,svg}",
+        url: "inline",
+        useHash: true,
+      },
+    ],
+    tailwindcss: {},
   },
 };
